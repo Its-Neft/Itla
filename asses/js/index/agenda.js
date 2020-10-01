d022d90e52b7bc8b1e2621bf3daf6a7632caea68
H++ <elnefta24@outlook.com>
2020-10-01 15:59:47 -0400

    Creacion de formulario y js
    Se creo el formulario con nombre, apellido, telefo y conacto.
    Se inicio la creacion de JavaScrip para validar la informacion

---
+++ asses/js/index/agenda.js
@@ -0,0 +1,12 @@
+
+//Funcion para agregar nombre
+
+function AgregarNombre(){
+    
+    //Variables
+    var inputtxtNombre = document.getElementById("txtNombre");
+    var txtNombre = inputtxtNombre.Value;
+    
+    //Metodos
+    alert(txtNombre);
+}
